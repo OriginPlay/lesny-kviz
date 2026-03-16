32009a35b1e440493766055396682b913aa5aefe
Whiteman <84192261+OriginPlay@users.noreply.github.com>
2026-03-16 10:47:16 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'lesny-kviz-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './js/html5-qrcode.min.js',
+  './images/home.jpg',
+  './images/path.jpg',
+  './images/quiz_default.jpg',
+  './images/finish.jpg',
+  './images/placeholder.jpg',
+  './images/stop_1.jpg',
+  './images/stop_2.jpg',
+  './images/stop_3.jpg',
+  './images/icon.png'
+];
+
+// Inštalácia - uloženie súborov do cache
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+// Aktivácia
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+// Načítavanie zo systému cache (offline prístup)
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
